dc5aa6831a68994cd4f58f49024e4a7275435e31
kordianbreitenbach <56912382+kordianbreitenbach@users.noreply.github.com>
2023-03-12 19:52:24 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
      <Router>
        <Navbar/>
       <Routes>
-   <Route path='/' element={<Shop/>} />
+   <Route path='/ecom' element={<Shop/>} />
     <Route path='/cart' element={<Cart/>}/>
     <Route path="/checkout" element={<Checkout />} />
 
